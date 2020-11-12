dd7c54b91c69d2195ec42588b4343623a1d6c202
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2020-11-12 15:18:52 -0400

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace PDF_Sample
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnPDF = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtWatermark = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkOpen = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.rbOrange = new System.Windows.Forms.RadioButton();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.rbPink = new System.Windows.Forms.RadioButton();
+            this.rbPurple = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPDF
+            // 
+            this.btnPDF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnPDF.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPDF.Location = new System.Drawing.Point(7, 166);
+            this.btnPDF.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPDF.Name = "btnPDF";
+            this.btnPDF.Size = new System.Drawing.Size(110, 35);
+            this.btnPDF.TabIndex = 1;
+            this.btnPDF.Text = "&Watermark PDF";
+            this.btnPDF.UseVisualStyleBackColor = false;
+            this.btnPDF.Click += new System.EventHandler(this.btnPDF_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Path";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(11, 48);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFilePath.Multiline = true;
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(340, 76);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // txtWatermark
+            // 
+            this.txtWatermark.AcceptsReturn = true;
+            this.txtWatermark.Location = new System.Drawing.Point(6, 42);
+            this.txtWatermark.Margin = new System.Windows.Forms.Padding(4);
+            this.txtWatermark.Multiline = true;
+            this.txtWatermark.Name = "txtWatermark";
+            this.txtWatermark.Size = new System.Drawing.Size(340, 76);
+            this.txtWatermark.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(7, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 500);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Arnold Bradshaw 2020";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnBrowse.Location = new System.Drawing.Point(10, 132);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(110, 35);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExit.Location = new System.Drawing.Point(275, 478);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 35);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(269, 30);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Watermark PDF Document";
+            // 
+            // chkOpen
+            // 
+            this.chkOpen.AutoSize = true;
+            this.chkOpen.Location = new System.Drawing.Point(124, 173);
+            this.chkOpen.Name = "chkOpen";
+            this.chkOpen.Size = new System.Drawing.Size(85, 21);
+            this.chkOpen.TabIndex = 3;
+            this.chkOpen.Text = "Open PDF";
+            this.chkOpen.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtFilePath);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Location = new System.Drawing.Point(16, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(369, 179);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select File";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbOrange);
+            this.groupBox2.Controls.Add(this.rbPurple);
+            this.groupBox2.Controls.Add(this.rbPink);
+            this.groupBox2.Controls.Add(this.rbRed);
+            this.groupBox2.Controls.Add(this.rbBlue);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnPDF);
+            this.groupBox2.Controls.Add(this.txtWatermark);
+            this.groupBox2.Controls.Add(this.chkOpen);
+            this.groupBox2.Location = new System.Drawing.Point(16, 251);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(369, 212);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Watermark";
+            // 
+            // rbRed
+            // 
+            this.rbRed.AutoSize = true;
+            this.rbRed.Location = new System.Drawing.Point(7, 127);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(49, 21);
+            this.rbRed.TabIndex = 6;
+            this.rbRed.TabStop = true;
+            this.rbRed.Text = "Red";
+            this.rbRed.UseVisualStyleBackColor = true;
+            // 
+            // rbOrange
+            // 
+            this.rbOrange.AutoSize = true;
+            this.rbOrange.Location = new System.Drawing.Point(70, 127);
+            this.rbOrange.Name = "rbOrange";
+            this.rbOrange.Size = new System.Drawing.Size(70, 21);
+            this.rbOrange.TabIndex = 6;
+            this.rbOrange.TabStop = true;
+            this.rbOrange.Text = "Orange";
+            this.rbOrange.UseVisualStyleBackColor = true;
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(154, 127);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(50, 21);
+            this.rbBlue.TabIndex = 6;
+            this.rbBlue.TabStop = true;
+            this.rbBlue.Text = "Blue";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            // 
+            // rbPink
+            // 
+            this.rbPink.AutoSize = true;
+            this.rbPink.Location = new System.Drawing.Point(218, 127);
+            this.rbPink.Name = "rbPink";
+            this.rbPink.Size = new System.Drawing.Size(49, 21);
+            this.rbPink.TabIndex = 6;
+            this.rbPink.TabStop = true;
+            this.rbPink.Text = "Pink";
+            this.rbPink.UseVisualStyleBackColor = true;
+            // 
+            // rbPurple
+            // 
+            this.rbPurple.AutoSize = true;
+            this.rbPurple.Location = new System.Drawing.Point(281, 127);
+            this.rbPurple.Name = "rbPurple";
+            this.rbPurple.Size = new System.Drawing.Size(63, 21);
+            this.rbPurple.TabIndex = 6;
+            this.rbPurple.TabStop = true;
+            this.rbPurple.Text = "Purple";
+            this.rbPurple.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 524);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Watermark PDF";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPDF;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtWatermark;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkOpen;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbOrange;
+        private System.Windows.Forms.RadioButton rbPurple;
+        private System.Windows.Forms.RadioButton rbPink;
+        private System.Windows.Forms.RadioButton rbRed;
+        private System.Windows.Forms.RadioButton rbBlue;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp.Drawing;
+using PdfSharp.Fonts;
+using PdfSharp.Internal;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf.Security;
+
+namespace PDF_Sample
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPDF_Click(object sender, EventArgs e)
+        {
+            // Variation 1: Draw a watermark as a text string
+            try
+            {
+                // Source File
+                string sourceFile = txtFilePath.Text;
+
+                // Take in pdf from the form
+                PdfDocument document = PdfReader.Open(sourceFile);
+                XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+                // Watermark String text
+                string watermark = txtWatermark.Text;
+
+                for (int idx = 0; idx < document.Pages.Count; idx++)
+                {
+                    var page = document.Pages[idx];
+
+                    // Get an XGraphics object for drawing beneath the existing content
+                    var gfx = XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Append);
+
+                    // Get the size (in points) of the text
+                    var size = gfx.MeasureString(watermark, font);
+
+                    // Define a rotation transformation at the center of the page
+                    gfx.TranslateTransform(page.Width / 2, page.Height / 2);
+                    gfx.RotateTransform(-Math.Atan(page.Height / page.Width) * 180 / Math.PI);
+                    gfx.TranslateTransform(-page.Width / 2, -page.Height / 2);
+
+                    // Create a string format
+                    var format = new XStringFormat();
+                    format.Alignment = XStringAlignment.Near;
+                    format.LineAlignment = XLineAlignment.Near;
+
+                    // Create a dimmed red brush
+                    int redColor;
+                    int greenColor;
+                    int blueColor;
+
+                    if (rbRed.Checked==true)
+                    {
+                        redColor = 255;
+                        greenColor = 102;
+                        blueColor = 102;
+                    }else if (rbOrange.Checked == true)
+                    {
+                        redColor = 255;
+                        greenColor = 204;
+                        blueColor = 153;
+                    }
+                    else if(rbBlue.Checked==true)
+                    {
+                        redColor = 0;
+                        greenColor = 128;
+                        blueColor = 255;
+                    }
+                    else if (rbPurple.Checked == true)
+                    {
+                        redColor = 204;
+                        greenColor = 153;
+                        blueColor = 255;
+                    }
+                    else
+                    {
+                        redColor = 255;
+                        greenColor = 153;
+                        blueColor = 255;
+                    }
+                    XBrush brush = new XSolidBrush(XColor.FromArgb(0, redColor, greenColor, blueColor));
+
+                    // Draw the string
+                    gfx.DrawString(watermark, font, brush,
+                    new XPoint((page.Width - size.Width) / 2, (page.Height - size.Height) / 2), format);
+
+                }
+
+                PdfSecuritySettings ss = document.SecuritySettings;
+                // Setting one of the passwords automatically sets the security level to
+                // PdfDocumentSecurityLevel.Encrypted128Bit.
+                ss.UserPassword = "";
+                ss.OwnerPassword = "mHfb6A7Rwp&b^$z2";
+
+                // Restrict some rights
+                ss.PermitAccessibilityExtractContent = false;
+                ss.PermitAnnotations = false;
+                ss.PermitAssembleDocument = false;
+                ss.PermitExtractContent = false;
+                ss.PermitFormsFill = true;
+                ss.PermitFullQualityPrint = true;
+                ss.PermitModifyDocument = false;
+                ss.PermitPrint = true;
+                string newFileName = sourceFile.Substring(0,sourceFile.Length-4) + " - WM.pdf";
+                
+                // Save document
+                document.Save(newFileName);
+                
+                // Release the document from memory
+                document.Close();
+
+                // Messagebox to show completed
+                MessageBox.Show("PDF Completed!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Open PDF on completion
+                if (chkOpen.Checked == true)
+                {
+                    Process.Start(newFileName);
+                }
+                else
+                {
+                    return;
+                }
+             }
+            catch (Exception ex)
+            {
+                // Trap any error messages
+                MessageBox.Show($"There was an error: '{ex}'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+             }
+                                  
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Default watermark string, loaded every time
+            string watermarkText = "RADIAN H.A. Limited";
+            txtWatermark.Text = watermarkText;
+            chkOpen.Checked = true;
+            rbRed.Checked = true;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            // Open file dialogue
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse PDF Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "pdf",
+                Filter = "pdf files (*.pdf)|*.pdf",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Select the file path and put in textbox
+                txtFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Exit application and release recources
+            Application.Exit();
+        }
+    }
+}
